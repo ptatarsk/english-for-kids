2e087a5233be717c4d86153188ef87c3484a7bd6
TatarskiyPavel <Pasha.Tatarsky@gmal.com>
2020-04-22 23:49:24 +0300

    feat: add favicon

--- english-for-kids/src/index.js
+++ english-for-kids/src/index.js
@@ -2,6 +2,11 @@ import Application from './js/Application';
 import 'bootstrap';
 
 window.onload = () => {
+  const link = document.createElement('link');
+  link.type = 'image/x-icon';
+  link.rel = 'shortcut icon';
+  link.href = './assets/icons/favicon.ico';
+  document.getElementsByTagName('head')[0].appendChild(link);
   const app = new Application();
   app.renderApp();
   app.initApp();
